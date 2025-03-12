bc28c1190982f0cdc4c7195c76b737d5bf9250e2
EcosystemNetwork.io <86893562+EcosystemNetwork@users.noreply.github.com>
2025-03-12 08:51:19 +0000

    Following videos

---
+++ Comic.java
@@ -0,0 +1,47 @@
+public class Comic {
+    //Instance Varibles 
+    private String writer;
+    private String illustrator;
+    private String colorist;
+    private String title;
+    private int issueNum;
+    //Mutators+Accessors
+    //Getter & setters for illustrator
+    public String getIllustrator() {
+        return illustrator;
+    }
+    public void setIllustrator(String illustrator) {
+        this.illustrator = illustrator;
+    }
+    //Getter & setters for writer
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+    //Getter & setters for colorist
+    public String getColorist() {
+            return colorist;
+        }
+    public void setColorist(String colorist) {
+        this.colorist = colorist;
+        }
+    //Getter & setters for title
+    public String getTitle() {
+            return title;
+        }
+
+    public void setTitle(String title) {
+        this.title = title;
+        }
+    //Getter & setters for issueNum
+    public int getIssueNum() {
+        return issueNum;
+    }
+
+    public void setTitle(int issueNum) {
+        this.issueNum = issueNum;
+    }   
+}
