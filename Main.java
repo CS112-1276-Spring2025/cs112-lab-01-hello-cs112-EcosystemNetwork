bbaa92cf1bc7e1fe15945856db3577b1d904054b
EcosystemNetwork.io <86893562+EcosystemNetwork@users.noreply.github.com>
2025-03-12 08:20:00 +0000

    Part 1

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS 112!");
+
+        System.out.println("Do I like baking?" + Main.isALike("baking"));
+        System.out.println("Do I like Fishing?" + Main.isALike("Fishing"));
+        Main.printAboutMe();
+    }
+
+    public static void printAboutMe() {
+        System.out.println("I like food, baking, cooking, etc.");
+    }
+
+    public static boolean isALike(String topic) {
+        String myLikes = "I like food, baking, cooking, etc.";
+        return myLikes.contains(topic);
     }
 }
