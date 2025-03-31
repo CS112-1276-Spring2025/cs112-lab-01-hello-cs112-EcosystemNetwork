705c2e4707eac886fb53718ac6b829ddceaedad6
EcosystemNetwork.io <86893562+EcosystemNetwork@users.noreply.github.com>
2025-03-31 07:53:31 +0000

    Finito

--- Comic.java
+++ Comic.java
@@ -1,19 +1,33 @@
+/*
+@Eric Nans
+@Version 1.0
+Lab 01 - Hello CS112
+Comic class that holds all the meta data to display in the mian program.
+*/
+
 public class Comic {
-    //Instance Varibles 
+    // Instance Variables
     private String writer;
     private String illustrator;
     private String colorist;
     private String title;
     private int issueNum;
-    //Mutators+Accessors
-    //Getter & setters for illustrator
-    public String getIllustrator() {
-        return illustrator;
-    }
-    public void setIllustrator(String illustrator) {
+
+    // Full Constructor
+    public Comic(String writer, String illustrator, String colorist, String title, int issueNum) {
+        this.writer = writer;
         this.illustrator = illustrator;
+        this.colorist = colorist;
+        this.title = title;
+        this.issueNum = issueNum;
     }
-    //Getter & setters for writer
+
+    // Default Constructor
+    public Comic() {
+        this("Jane Doe", "John Doe", "Jorge Doe", "Comic", 0);
+    }
+
+    // Getters and Setters
     public String getWriter() {
         return writer;
     }
@@ -21,22 +35,31 @@ public class Comic {
     public void setWriter(String writer) {
         this.writer = writer;
     }
-    //Getter & setters for colorist
+
+    public String getIllustrator() {
+        return illustrator;
+    }
+
+    public void setIllustrator(String illustrator) {
+        this.illustrator = illustrator;
+    }
+
     public String getColorist() {
-            return colorist;
-        }
+        return colorist;
+    }
+
     public void setColorist(String colorist) {
         this.colorist = colorist;
-        }
-    //Getter & setters for title
+    }
+
     public String getTitle() {
-            return title;
-        }
+        return title;
+    }
 
     public void setTitle(String title) {
         this.title = title;
-        }
-    //Getter & setters for issueNum
+    }
+
     public int getIssueNum() {
         return issueNum;
     }
@@ -44,17 +67,26 @@ public class Comic {
     public void setIssueNum(int issueNum) {
         this.issueNum = issueNum;
     }
-    
-    // Override toString method to provide a string representation of the Comic object.
 
+    // toString method
     @Override
     public String toString() {
         return "Comic{" +
-               "illustrator={'" + illustrator + "\'" +
-               ", writer={'" + writer + "\'" +
-               ", colorist={'" + colorist + "\'" +
-               ", title={'" + title + "\'" +
-                ", issueNum={'" + issueNum + "\'" + 
+                "writer='" + writer + '\'' +
+                ", illustrator='" + illustrator + '\'' +
+                ", colorist='" + colorist + '\'' +
+                ", title='" + title + '\'' +
+                ", issueNum=" + issueNum +
                 '}';
+    }
+
+    // equals method
+    public boolean equals(Comic otherComic) {
+        if (otherComic == null) return false;
+        return this.writer.equals(otherComic.writer) &&
+               this.illustrator.equals(otherComic.illustrator) &&
+               this.colorist.equals(otherComic.colorist) &&
+               this.title.equals(otherComic.title) &&
+               this.issueNum == otherComic.issueNum;
     }
 }
--- Main.java
+++ Main.java
@@ -1,22 +1,40 @@
+/*
+@Eric Nans
+@Version 1.0
+Lab 01 - Hello CS112
+Small program to learn about the basic foundatin of Java. 
+*/
+
 public class Main {
     public static void main(String[] args) {
+        // Part 1: Simple output
         System.out.println("Welcome to CS 112!");
 
-        System.out.println("Do I like baking?" + Main.isALike("baking"));
-        System.out.println("Do I like Fishing?" + Main.isALike("Fishing"));
+        // Part 2: Static methods in action
+        System.out.println("Do I like baking? " + Main.isALike("baking"));
+        System.out.println("Do I like fishing? " + Main.isALike("fishing"));
         Main.printAboutMe();
-        Comic favorite = new Comic();
-        favorite.setTitle("Superman 1111");
-        favorite.setIssueNum(1);
+
+        // Part 3: Object-Oriented Programming with the Comic class
+        Comic favorite = new Comic("Stan Lee", "Steve Ditko", "John Higgins", "Spider-Man", 1);
+        favorite.setTitle("Superman #1111");  // using setter
+        favorite.setIssueNum(1111);          // using setter
+
         System.out.println("My favorite comic is: " + favorite.getTitle());
+        System.out.println(favorite.toString());
+
+        Comic anotherComic = new Comic("Stan Lee", "Steve Ditko", "John Higgins", "Superman #1111", 1111);
+        System.out.println("Are the comics equal? " + favorite.equals(anotherComic));
     }
 
+    // Void static method
     public static void printAboutMe() {
         System.out.println("I like food, baking, cooking, etc.");
     }
 
+    // Static method with a parameter and a return value
     public static boolean isALike(String topic) {
         String myLikes = "I like food, baking, cooking, etc.";
-        return myLikes.contains(topic);
+        return myLikes.toLowerCase().contains(topic.toLowerCase());
     }
 }
